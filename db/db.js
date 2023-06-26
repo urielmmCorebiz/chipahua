db15c06e815c04f4e2bddc3b5a81af444081c457
urieeldev <uriel.mejia@corebiz.ag>
2023-06-26 13:27:44 -0600

    Feat: se cambian las imagenes de servicios

--- db/db.js
+++ db/db.js
@@ -1,34 +1,19 @@
 export const infoCards = [
   {
     title: "Chipahua",
-    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/appCehf%2FIMG-20230130-WA0025.jpg?alt=media&token=eb6e3547-ab99-431b-a6af-f314aa059d2a",
+    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/landings%2Ffoto001.png?alt=media&token=94efd515-9982-42e8-b32e-4e09e6689363",
     description: "",
   },
   {
     title: "Chipahua",
-    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/appCehf%2FIMG-20230130-WA0039.jpg?alt=media&token=ffc0c787-fab9-4d13-a328-63520780538e",
+    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/landings%2Ffoto002.png?alt=media&token=ac1ad867-d925-4e79-b72e-82fde6b9ff62",
     description: "",
   },
   {
     title: "Chipahua",
-    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/appCehf%2FIMG-20230130-WA0036.jpg?alt=media&token=a5155e31-a2b2-4ce9-a46f-a85686c3983e",
+    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/landings%2Ffoto003.png?alt=media&token=18055782-e18a-4dda-acc8-aede2d8225b7",
     description: "",
-  },
-  {
-    title: "Chipahua",
-    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/appCehf%2FIMG-20230130-WA0034.jpg?alt=media&token=0a195a39-0f25-406b-9b2e-e5bd4451bb3f",
-    description: "",
-  },
-  {
-    title: "Chipahua",
-    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/appCehf%2FIMG-20230130-WA0033.jpg?alt=media&token=cd4f7531-ff42-4e43-ad1d-211de2927c9e",
-    description: "",
-  },
-  {
-    title: "Chipahua",
-    img: "https://firebasestorage.googleapis.com/v0/b/cehf-mx.appspot.com/o/appCehf%2FIMG-20230130-WA0028.jpg?alt=media&token=622daeb9-fb85-4576-9845-84f73ed95f0e",
-    description: "",
-  },
+  }
 ];
 
 export const infoCardServices = [
